ed898b6ce29e2b9e833aa38bcbb96dd2e254ced5
LGD-P <nplgdjp@gmx.com>
2022-11-27 14:43:14 +0100

    Black and ETL display

--- display_answer.py
+++ display_answer.py
@@ -9,26 +9,30 @@ table = Table(
     "**\n[/u bold red] \n"
     "[u bold yellow]Un fichier OUTPUT contient le résultat dans des sous dossiers CSV "
     "et IMG:[/u bold yellow]\n\n"
-    
 )
 
-table.add_column("FICHIER .CSV",justify="center",style="dark_blue",no_wrap=False,
-                 header_style="orange3",)
+table.add_column(
+    "FICHIER .CSV",
+    justify="center",
+    style="dark_blue",
+    no_wrap=False,
+    header_style="orange3",
+)
 
-table.add_column("IMAGES", style="dark_blue", justify="center", no_wrap=False, 
-                 header_style="orange3")
+table.add_column(
+    "IMAGES", style="dark_blue", justify="center", no_wrap=False, header_style="orange3"
+)
 
 table.add_row(
     "[left]- Dans le sous dossier  [bold blue u i]CSV:[/bold blue u i].\n\n"
     "- L'intégralité des données du site:\n"
     "[bold green u i]https://books.toscrape.com/index.html[/bold green u i]\n\n"
     "- Ont été stockées dans un fichier [bold red u i]books-listing.csvdans"
-    "[/bold red u i] un fichier csv.[/left]",  
-    
+    "[/bold red u i] un fichier csv.[/left]",
     "- Les images des livres ont toutes été téléchargées dans un sous  dossier "
     " [bold blue u i]IMG:[/bold blue u i] \n\n"
     "- Chaque image est classée dans la catégorie du livre qui lui correspond\n\n"
-    "- Elles sont identifiables grace à leur [bold red u i]N°UPC[/bold red u i]"
+    "- Elles sont identifiables grace à leur [bold red u i]N°UPC[/bold red u i]",
 )
 
 
@@ -43,7 +47,6 @@ def return_pretty_message(table):
     Returns:
        2 columns to describe the content
         of the files created according to
-        their category .csv or .jpg       
+        their category .csv or .jpg
     """
     return c.print(table)
-
--- dl_img_and_get_data_to_csv.py
+++ dl_img_and_get_data_to_csv.py
@@ -21,12 +21,12 @@ current_path = Path.cwd()
 
 OUTPUT_FOLDER = current_path / "OUTPUT"
 
-IMG_FOLDER =  OUTPUT_FOLDER / "IMG"
+IMG_FOLDER = OUTPUT_FOLDER / "IMG"
 
-CSV_FOLDER =  OUTPUT_FOLDER / "CSV"
+CSV_FOLDER = OUTPUT_FOLDER / "CSV"
 
 
-def creat_dir_and_sub_dir(path_1,path_2,path_3):
+def creat_dir_and_sub_dir(path_1, path_2, path_3):
     """This function creat directory
 
     Args:
@@ -34,11 +34,10 @@ def creat_dir_and_sub_dir(path_1,path_2,path_3):
         path_2 (path): IMG SUB-DIR
         path_3 (path): CSV SUB-DIR
     """
-    path_1.mkdir(exist_ok= True)
-    path_2.mkdir(exist_ok= True)
-    path_3.mkdir(exist_ok= True)
+    path_1.mkdir(exist_ok=True)
+    path_2.mkdir(exist_ok=True)
+    path_3.mkdir(exist_ok=True)
 
- 
 
 def write_books_csv_file(dictionary):
     """This function use all books dictionary to
@@ -47,23 +46,25 @@ def write_books_csv_file(dictionary):
     Args:
         dictionnary (dict): all books in dictionnary
     """
-    with open(CSV_FOLDER / "books-listing.csv","w",newline="",encoding="utf-8") as books_listing:
+    with open(
+        CSV_FOLDER / "books-listing.csv", "w", newline="", encoding="utf-8"
+    ) as books_listing:
         writer = csv.DictWriter(books_listing, fieldnames=DATA_TO_SCRAPE)
         writer.writeheader()
         writer.writerows(dictionary)
-    
+
 
 def dl_img_in_sub_dir(dictionary):
-    """This function download all books images. 
+    """This function download all books images.
     all images are put in subdirectory names by category
 
     Args:
         dictionary (dict): all books in dictionnary
     """
-    for element in track(dictionary,description="TRANSFORM data"):
+    for element in track(dictionary, description="LOAD data"):
         folder_path = IMG_FOLDER / element["CATEGORY"]
-        folder_path.mkdir(exist_ok=True) 
+        folder_path.mkdir(exist_ok=True)
         urllib.request.urlretrieve(
             element["IMAGE_URL"],
-            f'{folder_path}/{element["CATEGORY"]} - {element["UNIVERSAL_PRODUCT_CODE"]}.jpg')
-
+            f'{folder_path}/{element["CATEGORY"]} - {element["UNIVERSAL_PRODUCT_CODE"]}.jpg',
+        )
